a125c3a3448d4e25fcaac45a0c61c63f9f915abc
Rushikesh Bagade <rbagade911@gmail.com>
2026-05-04 16:13:08 +0530

    remove .next from repo

--- .next/static/chunks/app/page-0d9b8525a667d36d.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[931],{863:function(n,e,u){Promise.resolve().then(u.bind(u,7669))}},function(n){n.O(0,[138,669,971,23,744],function(){return n(n.s=863)}),_N_E=n.O()}]);
